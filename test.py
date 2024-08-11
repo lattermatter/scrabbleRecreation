bfa21394cff41ad8e32b711bd4d670018a5d72d9
lattermatter <116776079+lattermatter@users.noreply.github.com>
2024-08-10 20:28:48 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
 set1 = {1,2,3,4}
 set2 = 7
 print(True) if 0 or 0 in set1 else print(False)
+
+class Test():
+    def __init__(self, woah) -> None:
+        self.woah = woah
+        self.apple = self.woah + 5
+    def get(self):
+        return self.apple
+
+
+
+obj = Test(2)
+print(obj.get())
+obj.woah = 5
+print(obj.get())
+
+# all object vars are not updated automatically upon changing a var
+
+def check(a):
+    b = a.copy()
+    b[0] = 7
+    print(a)
+    
+check([1,2])
